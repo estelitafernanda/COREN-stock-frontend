1269b557569de8f0d1254bd8c66f5536e63d7e2e
augustoams01 <augustoams01@gmail.com>
2024-11-22 13:25:31 -0300

    dashboard-upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client';
 import { FaHeadset } from "react-icons/fa";
+import ChartPieDash from "@/components/dash/ChartPieDash";
 
 
 export default function Home() {
   return (
-    <div className="mx-auto w-[95vw] mt-10 flex justify-center min-h-full font-[family-name:var(--font-geist-sans)]">
+    <div className="mx-auto w-[95vw] mt-7 flex flex-col justify-center min-h-full font-[family-name:var(--font-geist-sans)]">
 
       <div className="flex justify-between w-full">
         <h1 className="text-3xl font-bold text-lightW">Dashboard</h1>
@@ -15,9 +17,52 @@ export default function Home() {
         </div>
       </div>
 
-      <section>
-        
-      </section>
+      <div className="grid grid-cols-7 gap-4 w-full h-72 mt-5">
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-4">
+          <h2 className="text-2xl font-bold">Stock Level:</h2>
+          <div className="flex gap-5">
+            <ChartPieDash/>
+            <div className="w-full">
+              <div className="flex flex-col">
+                <p>High stock product</p>
+                <div className="w-full h-2 bg-primary rounded-full"></div>
+                <p>1200 products</p>
+              </div>
+              <div>
+                <p>near-low stock product</p>
+                <div className="w-full h-2 bg-primary rounded-full"></div>
+                <p>1200</p>
+              </div>
+              <div>
+                <p>low stock product</p>
+                <div className="w-full h-2 bg-primary rounded-full"></div>
+                <p>1200</p>
+              </div>
+              <div>
+                <p>out of stock product</p>
+                <div className="w-full h-2 bg-primary rounded-full"></div>
+                <p>1200</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-3"></div>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4 w-full h-40 mt-5">
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-1"></div>
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-1"></div>
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-1"></div>
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-1"></div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 w-full h-60 mt-5">
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-1"></div>
+        <div className="bg-blackSecondary p-5 rounded-lg col-span-1">
+          <div className="max-w-60"></div>
+        </div>
+      </div>
 
     </div>
   );
---
+++ src/components/dash/ChartPieDash.tsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from 'react';
+import { ApexOptions } from 'apexcharts';
+import dynamic from 'next/dynamic';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false,
+})
+
+const options: ApexOptions = {
+    chart: {
+        foreColor: "#fff",
+    },
+
+
+    legend: {
+        show: true,
+        showForSingleSeries: false,
+        showForNullSeries: true,
+        showForZeroSeries: true,
+        position: 'right',
+        horizontalAlign: 'center', 
+        floating: false,
+        fontSize: '14px',
+        fontFamily: 'Helvetica, Arial',
+        fontWeight: 700,
+        formatter: undefined,
+        inverseOrder: false,
+        width: undefined,
+        height: undefined,
+        tooltipHoverFormatter: undefined,
+        customLegendItems: [],
+        offsetX: 0,
+        offsetY: 10,
+        labels: {
+            colors: undefined,
+            useSeriesColors: false
+        },
+        markers: {
+            size: 7,
+            shape: undefined,
+            strokeWidth: 0,
+            fillColors: undefined,
+            customHTML: undefined,
+            onClick: undefined,
+            offsetX: -10,
+            offsetY: 0
+        },
+        itemMargin: {
+            horizontal: 0,
+            vertical: 10
+        },
+        onItemClick: {
+            toggleDataSeries: true
+        },
+        onItemHover: {
+            highlightDataSeries: true
+        },
+    },
+    
+
+    xaxis: {    
+        type: 'datetime',
+        axisBorder: {
+            color: "#fff"
+        },
+        axisTicks: {
+            color: "#fff"
+        },
+        categories: [
+            '2021-03-18T00:00:00.000Z',
+            '2021-03-19T00:00:00.000Z',
+            '2021-03-20T00:00:00.000Z',
+            '2021-03-21T00:00:00.000Z',
+            '2021-03-22T00:00:00.000Z',
+        ]
+    },
+
+    series: [44, 55, 13, 33],
+    
+    stroke: {
+        show: false,
+    },   
+
+    labels: ['Apple', 'Mango', 'Orange', 'Watermelon'],  
+    dataLabels: {
+        enabled: false,
+    },
+
+    fill: {
+        colors: ['#39db7d','#ffeb39','#fd3b75','#56cbec'],
+        opacity: 1,
+    },
+
+    plotOptions: {
+        pie: {
+            startAngle: 0,
+            endAngle: 360,
+            expandOnClick: true,
+            offsetX: 0,
+            offsetY: 0,
+            customScale: 1,
+          donut: {
+            labels: {
+              show: true,
+              name: {
+                show: true,
+                fontSize: '18px',
+                fontFamily: 'Helvetica, Arial, sans-serif',
+                fontWeight: 600,
+                color: "#"
+              },
+              value: {
+                show: true,
+                fontSize: '38px',
+                fontFamily: 'sans-serif',
+                fontWeight: 600,
+                color: '#fff'
+              },
+              total: {
+                show: true,
+                fontSize: '18px',
+                fontFamily: 'Helvetica, Arial, sans-serif',
+                fontWeight: 600,
+                color: '#758694'
+              }
+            }
+          }
+        }
+    }
+}
+
+function ChartPieDash() {
+    
+    const [chartState, setChartState] = useState<boolean>(false)
+
+    useEffect(() => {
+        setChartState(true)
+    }, [])
+
+  return (
+    <div className='-ml-10 mt-3'>
+        { chartState && <Chart type={'donut'} width={400} height={210} series={options.series} options={options} />}
+    </div>
+  )
+}
+
+export default ChartPieDash
