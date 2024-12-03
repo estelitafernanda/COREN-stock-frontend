061cdfdaca21ce0e95fbc2b56b3dbf7c122d6105
augustoams01 <augustoams01@gmail.com>
2024-12-03 12:06:40 -0300

    search-bar-commit

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,19 +1,28 @@
 import Image from 'next/image'
 import React from 'react'
 import { FaHeadset } from 'react-icons/fa6'
-import { IoIosArrowDown } from 'react-icons/io'
+import { IoIosAdd } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
+
 import Trash from "../../../public/lixeira.png";
 
 function Inventory() {
   return (
     <div className="mx-auto w-[95vw] mt-7 flex flex-col justify-center min-h-full font-[family-name:var(--font-geist-sans)]">
       <div className="flex justify-between w-full">
-        <h1 className="text-3xl font-bold text-lightW">Inventario</h1>
+        <div className='flex items-center gap-5'>
+          <h1 className="text-3xl font-bold text-lightW">Inventario</h1>
+          <p className='text-sm font-bold text-lightW/30 bg-lightW/10 px-3 py-1 rounded-full border border-lightW/30'>Total de produtos: <span className='text-lightW'>386</span></p>
+        </div>
+        <div className=' flex items-center bg-blackSecondary border border-lightW/30 p-5 rounded-lg w-[30%] h-3 gap-2'>
+          <FaSearch size={20} className='text-lightW/30'/>
+          <p className='text-sm font-bold text-lightW/30'>buscar</p>
+        </div>
         <div className="flex gap-4">
           <button className="hover:bg-primary group hover:text-lightW flex gap-1 border-[1px] border-primary py-2 px-5 rounded-lg text-primary text-md font-semibold transition duration-300">
             <FaHeadset size={20} className="hover:text-lightW"/> Contato com suporte
           </button>
-          <button className="border gap-1 items-center border-primary bg-primary transition duration-300 hover:bg-transparent hover:text-primary flex py-2 px-5 rounded-lg text-md font-semibold text-blackPrimary">Adicionar Produto <IoIosArrowDown/></button>
+          <button className="border gap-1 items-center border-primary bg-primary transition duration-300 hover:bg-transparent hover:text-primary flex py-2 px-5 rounded-lg text-md font-semibold text-blackPrimary">Adicionar Produto <IoIosAdd size={20}/></button>
         </div>
       </div>
       <section className='h-[80vh] flex gap-5 mt-5'>
@@ -51,7 +60,7 @@ function Inventory() {
         </div>
 
         <div className='bg-blackSecondary p-5 rounded-lg w-[75%]'>
-          <div className="flex bg-blackThirdy gap-3 p-2 rounded-lg">
+          <div className="border border-transparent hover:border-primary transition duration-300 flex bg-blackThirdy gap-3 p-2 rounded-lg">
                 <div className="bg-lightW/90 h-16 w-16 rounded-lg flex items-center justify-center">
                   <Image src={Trash} alt="Avatar" width={50} height={50}/>
                 </div>
